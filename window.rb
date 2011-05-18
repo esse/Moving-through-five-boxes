25e38abae4bbdaa5ac1cd987e63015be08ac2db4
Piotr Szmielew <p.szmielew@beinformed.pl>
2011-05-18 02:35:47 +0200

    chodzenie i obracanie jedna bryla

--- window.rb
+++ window.rb
@@ -81,32 +81,103 @@ end
 class GameWindow < Gosu::Window
   def initialize
     super 640, 480, false
-    self.caption = "Gosu Tutorial Game"
+    self.caption = "Projekt Grafika 2"
     @image = TexPlay.create_image(self, 640, 480, :color => Gosu::Color::BLUE) 
     @vertices = [
-                Point3d.new(-1,1,-1),
-                Point3d.new(1,1,-1),
-                Point3d.new(1,-1,-1),
-                Point3d.new(-1,-1,-1),
-                Point3d.new(-1,1,1),
-                Point3d.new(1,1,1),
-                Point3d.new(1,-1,1),
-                Point3d.new(-1,-1,1)
+      #pierwsza kostka
+                Point3d.new(-1,1,-1), #0
+                Point3d.new(1,1,-1), #1
+                Point3d.new(1,-1,-1), #2
+                Point3d.new(-1,-1,-1), #3
+                Point3d.new(-1,1,1), #4
+                Point3d.new(1,1,1), #5
+                Point3d.new(1,-1,1), #6
+                Point3d.new(-1,-1,1), #7
+      #druga kostka
+                Point3d.new(-2,2,-2), #8
+                Point3d.new(2,2,-2), #9
+                Point3d.new(2,-2,-2), #10
+                Point3d.new(-2,-2,-2), #11
+                Point3d.new(-2,2,2), #12
+                Point3d.new(2,2,2), #13
+                Point3d.new(2,-2,2), #14
+                Point3d.new(-2,-2,2) #15
             ]
-    @faces = [[0,1,2,3],[1,5,6,2],[5,4,7,6],[4,0,3,7],[0,4,5,1],[3,2,6,7]]
-    @queue = []
+    @faces = [
+      #pierwsza kostka
+      [0,1,2,3],[1,5,6,2],[5,4,7,6],[4,0,3,7],[0,4,5,1],[3,2,6,7],
+      #druga kostka
+    #  [8,9,11,12] ,[9,13,14,10],[13,12,11,10],[12,8,11,15],[8,12,13,9],[11,10,14,15]
+    ]
+    @distance = 4
   end
 
   def update
-    if button_down? Gosu::KbLeft or button_down? Gosu::GpLeft then
-      p "lewo!"
-      @queue << "rotX"
+    if button_down? Gosu::KbE then
+      @vertices = @vertices.map do |v|
+        v.rotateX(10)
+      end
     end
-    if button_down? Gosu::KbRight or button_down? Gosu::GpRight then
-      @queue << "rotY"
+    if button_down? Gosu::KbD then
+      @vertices = @vertices.map do |v|
+        v.rotateX(-10)
+      end
     end
-    if button_down? Gosu::KbUp or button_down? Gosu::GpButton0 then
-      @queue << "rotZ"
+    if button_down? Gosu::KbQ then
+      @vertices = @vertices.map do |v|
+        v.rotateY(10)
+      end
+    end
+    if button_down? Gosu::KbW then
+      @vertices = @vertices.map do |v|
+        v.rotateY(-10)
+      end
+    end 
+    if button_down? Gosu::KbC then
+      @vertices = @vertices.map do |v|
+        v.rotateZ(10)
+      end
+    end
+    if button_down? Gosu::KbV then
+      @vertices = @vertices.map do |v|
+        v.rotateZ(-10)
+      end
+    end
+    if button_down? Gosu::KbUp then
+      @vertices = @vertices.map do |v|
+        Point3d.new(v.x, v.y+0.1, v.z)
+      end
+    end
+    if button_down? Gosu::KbDown then
+    #  @distance -= 0.1
+    @vertices = @vertices.map do |v|
+      Point3d.new(v.x, v.y-0.1, v.z)
+    end
+    end
+    if button_down? Gosu::KbLeft then
+      @vertices = @vertices.map do |v|
+        Point3d.new(v.x - 0.1, v.y, v.z)
+      end
+    end
+    if button_down? Gosu::KbRight then
+      @vertices = @vertices.map do |v|
+        Point3d.new(v.x + 0.1, v.y, v.z)
+      end
+    end
+    if button_down? Gosu::KbO then
+    @vertices = @vertices.map do |v|
+        Point3d.new(v.x, v.y, v.z-0.1)
+    end
+    #@distance -= 0.1
+    end
+    if button_down? Gosu::KbL then
+    @vertices = @vertices.map do |v|
+      Point3d.new(v.x, v.y, v.z+0.1)
+    end
+    #@distance += 0.1
+    end
+    if button_down? Gosu::KbEscape then
+      close
     end
   end
 
@@ -128,42 +199,14 @@ class GameWindow < Gosu::Window
       #  line @projection7[:x], @projection7[:x], @projection0[:x], @projection0[:y]
   #  }
   t = []
-  if @queue.blank?
-    @vertices.each do |v|
+  @vertices.each do |v|
       # Rotate the point around X axis, then around Y axis, and finally around Z axis.
       #  r = v.rotateX(self.angleX).rotateY(self.angleY).rotateZ(self.angleZ)
       # Transform the point from 3D to 2D
-      p = v.project(640, 480, 256, 4)
+    p = v.project(640, 480, 256, @distance)
       # Put the point in the list of transformed vertices
-      t << p
-    end
-  else
-    transform = @vertices
-    @queue.each do |q|
-      tr = []
-      if q == "rotX"
-        transform.each do |v|
-          tr << v.rotateX(10)
-        end
-      elsif q == "rotY"
-        transform.each do |v|
-          tr << v.rotateY(10)
-        end
-        
-      elsif q == "rotZ"
-        transform.each do |v|
-          tr << v.rotateZ(10)
-        end    
-      end
-      transform = tr
-    end
-    transform.each do |v|
-      p = v.project(640, 480, 256, 4)
-      t << p
-    end
-    @vertices = transform
+    t << p
   end
-    @queue = []
     @faces.each do |f|
       @image.paint {
         line t[f[0]].x, t[f[0]].y, t[f[1]].x, t[f[1]].y
